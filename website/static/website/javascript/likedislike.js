5c744500cff86638777cd3b2623904289b0ab134
Patriks Blauzda <pblauzda@gmail.com>
2023-04-25 18:18:38 +0300

    post text formatting (unfinished)

--- website/static/website/javascript/likedislike.js
+++ website/static/website/javascript/likedislike.js
@@ -15,139 +15,139 @@ function getCookie(name) {
 }
 
 function like_game(game_id){
-        const url = "http://" + location.hostname + ":" + location.port + "/like/game/" + game_id + "/";
-        fetch(url)
-        .then(res => {
-            if(res.status == 201) {
-                res.json().then(
-                    val => {
-                        const score = document.getElementById("score" + game_id);
-                        const like_button = document.getElementById("like" + game_id);
-                        const dislike_button = document.getElementById("dislike" + game_id);
+    const url = "http://" + location.hostname + ":" + location.port + "/like/game/" + game_id + "/";
+    fetch(url)
+    .then(res => {
+        if(res.status == 201) {
+            res.json().then(
+                val => {
+                    const score = document.getElementById("score" + game_id);
+                    const like_button = document.getElementById("like" + game_id);
+                    const dislike_button = document.getElementById("dislike" + game_id);
 
-                        score.textContent = val.like;
+                    score.textContent = val.like;
 
-                        if(val.has_liked) {
-                            like_button.style.color = "#00b300";
+                    if(val.has_liked) {
+                        like_button.style.color = "#00b300";
 
-                            if(dislike_button.style.color != "rgb(0, 0, 0)") {
-                                dislike_button.style.color = "#000000";
-                            }
+                        if(dislike_button.style.color != "rgb(0, 0, 0)") {
+                            dislike_button.style.color = "#000000";
+                        }
 
-                        } else {
-                            like_button.style.color = "#000000";
+                    } else {
+                        like_button.style.color = "#000000";
 
-                            if(dislike_button.style.color != "rgb(0, 0, 0)") {
-                                dislike_button.style.color = "#000000";
-                            }
+                        if(dislike_button.style.color != "rgb(0, 0, 0)") {
+                            dislike_button.style.color = "#000000";
                         }
                     }
-                )
-            }
+                }
+            )
+        }
 
-        });
+    });
 }
 
 function dislike_game(game_id){
-        const url = "http://" + location.hostname + ":" + location.port + "/dislike/game/" + game_id + "/";
-        fetch(url)
-        .then(res => {
-            if(res.status == 201) {
-                res.json().then(
-                    val => {
-                        const score = document.getElementById("score" + game_id);
-                        const like_button = document.getElementById("like" + game_id);
-                        const dislike_button = document.getElementById("dislike" + game_id);
+    const url = "http://" + location.hostname + ":" + location.port + "/dislike/game/" + game_id + "/";
+    fetch(url)
+    .then(res => {
+        if(res.status == 201) {
+            res.json().then(
+                val => {
+                    const score = document.getElementById("score" + game_id);
+                    const like_button = document.getElementById("like" + game_id);
+                    const dislike_button = document.getElementById("dislike" + game_id);
 
-                        score.textContent = val.like;
+                    score.textContent = val.like;
 
-                        if(val.has_disliked) {
-                            dislike_button.style.color = "#b30000";
+                    if(val.has_disliked) {
+                        dislike_button.style.color = "#b30000";
 
-                            if(like_button.style.color != "rgb(0, 0, 0)") {
-                                like_button.style.color = "#000000";
-                            }
+                        if(like_button.style.color != "rgb(0, 0, 0)") {
+                            like_button.style.color = "#000000";
+                        }
 
-                        } else {
-                            dislike_button.style.color = "#000000";
+                    } else {
+                        dislike_button.style.color = "#000000";
 
-                            if(like_button.style.color != "rgb(0, 0, 0)") {
-                                like_button.style.color = "#000000";
-                            }
+                        if(like_button.style.color != "rgb(0, 0, 0)") {
+                            like_button.style.color = "#000000";
                         }
                     }
-                )
-            }
+                }
+            )
+        }
 
-        });
+    });
 }
 
 
 function like_post(post_id){
-        const url = "http://" + location.hostname + ":" + location.port + "/like/post/" + post_id + "/";
-        fetch(url)
-        .then(res => {
-            if(res.status == 201) {
-                res.json().then(
-                    val => {
-                        const score = document.getElementById("score" + post_id);
-                        const like_button = document.getElementById("like" + post_id);
-                        const dislike_button = document.getElementById("dislike" + post_id);
+    const url = "http://" + location.hostname + ":" + location.port + "/like/post/" + post_id + "/";
+    fetch(url)
+    .then(res => {
+        if(res.status == 201) {
+            res.json().then(
+                val => {
+                    const score = document.getElementById("score" + post_id);
+                    const like_button = document.getElementById("like" + post_id);
+                    const dislike_button = document.getElementById("dislike" + post_id);
 
-                        score.textContent = val.like;
+                    score.textContent = val.like;
 
-                        if(val.has_liked) {
-                            like_button.style.color = "#00b300";
+                    if(val.has_liked) {
+                        like_button.style.color = "#00b300";
 
-                            if(dislike_button.style.color != "rgb(0, 0, 0)") {
-                                dislike_button.style.color = "#000000";
-                            }
+                        if(dislike_button.style.color != "rgb(0, 0, 0)") {
+                            dislike_button.style.color = "#000000";
+                        }
 
-                        } else {
-                            like_button.style.color = "#000000";
+                    } else {
+                        like_button.style.color = "#000000";
 
-                            if(dislike_button.style.color != "rgb(0, 0, 0)") {
-                                dislike_button.style.color = "#000000";
-                            }
+                        if(dislike_button.style.color != "rgb(0, 0, 0)") {
+                            dislike_button.style.color = "#000000";
                         }
                     }
-                )
-            }
+                }
+            )
+        }
 
-        });
+    });
 }
 
 
 function dislike_post(post_id){
-        const url = "http://" + location.hostname + ":" + location.port + "/dislike/post/" + post_id + "/";
-        fetch(url)
-        .then(res => {
-            if(res.status == 201) {
-                res.json().then(
-                    val => {
-                        const score = document.getElementById("score" + post_id);
-                        const like_button = document.getElementById("like" + post_id);
-                        const dislike_button = document.getElementById("dislike" + post_id);
+    const url = "http://" + location.hostname + ":" + location.port + "/dislike/post/" + post_id + "/";
+    fetch(url)
+    .then(res => {
+        if(res.status == 201) {
+            res.json().then(
+                val => {
+                    const score = document.getElementById("score" + post_id);
+                    const like_button = document.getElementById("like" + post_id);
+                    const dislike_button = document.getElementById("dislike" + post_id);
 
-                        score.textContent = val.like;
+                    score.textContent = val.like;
 
-                        if(val.has_disliked) {
-                            dislike_button.style.color = "#b30000";
+                    if(val.has_disliked) {
+                        dislike_button.style.color = "#b30000";
 
-                            if(like_button.style.color != "rgb(0, 0, 0)") {
-                                like_button.style.color = "#000000";
-                            }
+                        if(like_button.style.color != "rgb(0, 0, 0)") {
+                            like_button.style.color = "#000000";
+                        }
 
-                        } else {
-                            dislike_button.style.color = "#000000";
+                    } else {
+                        dislike_button.style.color = "#000000";
 
-                            if(like_button.style.color != "rgb(0, 0, 0)") {
-                                like_button.style.color = "#000000";
-                            }
+                        if(like_button.style.color != "rgb(0, 0, 0)") {
+                            like_button.style.color = "#000000";
                         }
                     }
-                )
-            }
+                }
+            )
+        }
 
-        });
+    });
 }
